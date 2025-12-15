1f274bdfb4d1d15fb45ab69263de124851fae3af
Vignesh M <167705838+vignesh-learning@users.noreply.github.com>
2025-12-15 18:39:53 +0530

    fix: casesensitive bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4154,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'cc444de18062.ngrok-free.app', // 👈 add your ngrok domain here
+    ],
+  },
 })
